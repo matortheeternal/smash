b1a14f208c19ad771a1fe3915d040ce1eb25aa91
Mator <why101@msn.com>
2016-01-25 16:44:20 -0800

    Created tag helper form

---
+++ frontend/msTagHelper.dfm
@@ -0,0 +1,62 @@
+object TagHelper: TTagHelper
+  Left = 0
+  Top = 0
+  Caption = 'Tag Helper'
+  ClientHeight = 412
+  ClientWidth = 334
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  Position = poMainFormCenter
+  OnClose = FormClose
+  OnCreate = FormCreate
+  OnShow = FormShow
+  PixelsPerInch = 96
+  TextHeight = 13
+  object lblPrompt: TLabel
+    Left = 8
+    Top = 8
+    Width = 185
+    Height = 13
+    Caption = 'Check the tags you want to {ACTION}'
+  end
+  object CheckList: TCheckListBox
+    Left = 8
+    Top = 27
+    Width = 318
+    Height = 346
+    Align = alCustom
+    Anchors = [akLeft, akTop, akRight, akBottom]
+    ItemHeight = 13
+    Sorted = True
+    TabOrder = 0
+  end
+  object btnCancel: TButton
+    Left = 251
+    Top = 379
+    Width = 75
+    Height = 25
+    Align = alCustom
+    Anchors = [akRight, akBottom]
+    Caption = 'Cancel'
+    ModalResult = 2
+    TabOrder = 1
+    ExplicitTop = 400
+  end
+  object btnOK: TButton
+    Left = 170
+    Top = 379
+    Width = 75
+    Height = 25
+    Align = alCustom
+    Anchors = [akRight, akBottom]
+    Caption = 'OK'
+    ModalResult = 1
+    TabOrder = 2
+    ExplicitTop = 400
+  end
+end
---
+++ frontend/msTagHelper.pas
@@ -0,0 +1,77 @@
+unit msTagHelper;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls, CheckLst,
+  // mte units
+  RttiTranslation,
+  // smash units
+  msFrontend;
+
+type
+  TTagHelper = class(TForm)
+    CheckList: TCheckListBox;
+    lblPrompt: TLabel;
+    btnCancel: TButton;
+    btnOK: TButton;
+    procedure FormShow(Sender: TObject);
+    procedure FormClose(Sender: TObject; var Action: TCloseAction);
+    procedure FormCreate(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+    slTags: TStringList;
+    iMode: Integer;
+  end;
+
+var
+  TagHelper: TTagHelper;
+
+implementation
+
+{$R *.dfm}
+
+procedure TTagHelper.FormClose(Sender: TObject; var Action: TCloseAction);
+var
+  i: Integer;
+begin
+  slTags.Clear;
+  if ModalResult = mrOK then
+    for i := 0 to Pred(CheckList.Items.Count) do begin
+      if CheckList.Checked[i] then
+        slTags.Add(CheckList.Items[i]);
+    end;
+end;
+
+procedure TTagHelper.FormCreate(Sender: TObject);
+begin
+  // do a translation dump?
+  if bTranslationDump then
+    TRttiTranslation.Save('lang\english.lang', self);
+
+  // load translation
+  TRttiTranslation.Load(language, self);
+end;
+
+procedure TTagHelper.FormShow(Sender: TObject);
+begin
+  // update lblPrompt
+  case iMode of
+    -1: begin
+      Caption := GetString('msTagH_RemoveTags');
+      lblPrompt.Caption := GetString('msTagH_PromptRemove');
+    end;
+    1: begin
+      Caption := GetString('msTagH_AddTags');
+      lblPrompt.Caption := GetString('msTagH_PromptAdd');
+    end;
+  end;
+
+  // populate checklist
+  CheckList.Items.Text := slTags.Text;
+end;
+
+end.
