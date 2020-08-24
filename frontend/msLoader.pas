a962d5b86fe568ba3873a518af3fe90721c98ef3
chuck <>
2020-08-24 16:18:06 -0400

    Turn off translation of localized strings
    No need to translate them since we don't show them in the smash UI.
    This seems to resolve the "No strings file for lstring" error.

--- frontend/msLoader.pas
+++ frontend/msLoader.pas
@@ -44,7 +44,7 @@ uses
   msCore,
   // mp forms
   // xEdit units
-  wbHelpers, wbInterface, wbImplementation, wbBSA,
+  wbHelpers, wbInterface, wbImplementation, wbLocalization, wbBSA,
   wbDefinitionsFNV, wbDefinitionsFO3, wbDefinitionsTES3, wbDefinitionsTES4,
   wbDefinitionsTES5, wbDefinitionsFO4;
 
@@ -106,6 +106,7 @@ begin
   wbEditAllowed := True;
   wbContainerHandler := wbCreateContainerHandler;
   wbContainerHandler._AddRef;
+  wbLocalizationHandler.NoTranslate := true;
 
   // INITIALIZE DEFINITIONS
   Logger.Write('GENERAL', 'Definitions', 'Using '+wbAppName+'Edit Definitions');
